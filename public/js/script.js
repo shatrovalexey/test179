b749d6b2ddc05d7209c56db2fe21393693b12e3d
Алексей <mail@ashatrov.ru>
2025-07-25 07:46:07 +0300

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,30 @@
-(({"document": doc, "location": loc,}) => {
-    doc.addEventListener("DOMContentLoaded", evt => {
-        const form = doc.querySelector(".search-form");
-        const queryInput = form.querySelector("*[type='search']");
-        const table = form.querySelector(".search-table");
-        const caption = table.querySelector("caption");
-        const trTpl = form.querySelector("tbody > template");
+(({"document": doc, "location": loc,}) => doc.addEventListener("DOMContentLoaded", evt => {
+    const form = doc.querySelector(".search-form");
+    const queryInput = form.querySelector("*[type='search']");
+    const table = form.querySelector(".search-table");
+    const caption = table.querySelector("caption");
+    const trTpl = form.querySelector("tbody > template");
 
-        form.addEventListener("submit", evt => {
-            evt.preventDefault();
+    form.addEventListener("submit", evt => {
+        evt.preventDefault();
 
-            form.querySelectorAll("tbody > tr")
-                .forEach(tr => tr.remove());
+        form.querySelectorAll("tbody > tr").forEach(tr => tr.remove());
 
-            const url = new URL(form.getAttribute("action"), loc.origin);
-            url.search = new URLSearchParams(new FormData(form));
+        const url = new URL(form.getAttribute("action"), loc.origin);
+        url.search = new URLSearchParams(new FormData(form));
 
-            fetch(url)
-                .then(data => data.json())
-                .then(data => data.reverse())
-                .then(data => data.forEach(item => {
-                    const tr = trTpl.content.cloneNode(true);
+        fetch(url)
+            .then(data => data.json())
+            .then(data => data.reverse())
+            .then(data => data.forEach(item => {
+                const tr = trTpl.content.cloneNode(true);
 
-                    tr.querySelectorAll("*[data-content]")
-                        .forEach(node => node.textContent = item[node.dataset.content]);
+                tr.querySelectorAll("*[data-content]")
+                    .forEach(node => node.textContent = item[node.dataset.content]);
 
-                    trTpl.after(tr);
-                }));
+                trTpl.after(tr);
+            }));
 
-            return false;
-        });
+        return false;
     });
-})(window);
+)})(window);
--- public/search.php
+++ public/search.php
@@ -9,6 +9,8 @@ $config = require_once('../.config.php');
 $query = Codec::getFts($_REQUEST['q'] ?? null);
 $dba = &$config['dba'];
 $search = &$dba['query']['search'];
-$args = $query ? [$search['find'], [':fts' => $query,],] : [$search['list'],];
+$args = [$search[$query ? 'find' : 'list'],];
+
+if ($query) $args[] = [':fts' => $query,];
 
 echo Codec::getEncodeJson((new DBA($dba))->query(... $args)->fetchAll(\PDO::FETCH_ASSOC));
